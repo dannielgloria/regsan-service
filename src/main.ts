eab7a487366cf59a596b25e753a7fbda33ae7996
dannielgloria <danniel.gloria@gmail.com>
2024-09-29 15:09:12 -0600

    update auth.controller.ts, auth.service.ts and main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('Regsan Service API')
     .setDescription('API documentation for Regsan Service')
